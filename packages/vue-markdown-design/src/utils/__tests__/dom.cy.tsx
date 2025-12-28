92be8188ab99f3ed5ed5fb583ebc4f444dc86e1e
Zero <zeronex@vip.qq.com>
2025-12-28 20:59:22 +0800

    feat: limit scroll behavior to innermost container only

--- packages/vue-markdown-design/src/utils/__tests__/dom.cy.tsx
+++ packages/vue-markdown-design/src/utils/__tests__/dom.cy.tsx
@@ -4,18 +4,16 @@ describe('dom', () => {
   it('scrollToEl', () => {
     cy.mount(() => (
       <>
-        <div style="height: 100vh;"></div>
         <div style="height: 300px; overflow: scroll;">
           <div style="height: 100vh;"></div>
           <div data-cy>content</div>
           <div style="height: 100vh;"></div>
         </div>
-        <div style="height: 100vh;"></div>
       </>
     ))
     cy.get('[data-cy]').should(($el) => {
       scrollToEl($el[0])
-      expect($el).to.boundary.satisfy(({ top }) => top === 0)
+      expect($el).to.boundary.satisfy(({ top }) => top === 8)
     })
   })
 })
