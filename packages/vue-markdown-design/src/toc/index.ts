98696e1af75cf134d21079c7b362fca5ef1d972b
Zero <zeronex@vip.qq.com>
2025-12-28 21:31:17 +0800

    feat(toc): add `VmdToc` export alias for better editor recognition

--- packages/vue-markdown-design/src/toc/index.ts
+++ packages/vue-markdown-design/src/toc/index.ts
@@ -2,6 +2,7 @@ import { withInstall } from '../utils/vue'
 import _TOC from './TOC'
 
 export const VmdTOC = withInstall(_TOC, ['VmdTOC'])
+export const VmdToc = VmdTOC
 export default VmdTOC
 export { tocProps } from './TOC'
 export type { TOCProps, TOCInstance, TOCItem } from './type'
